503aaafb7f4467de093f1a6f8d6bc4bf6099abf3
ddock4you <ddock4you@codeline.kr>
2021-02-05 17:40:08 +0900

    - nodebird 세팅 중

---
+++ nodebird/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname + 'public')));
